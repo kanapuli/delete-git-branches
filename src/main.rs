29cf25d93622c082a668e3461d8a1c7a0028e88f
kanapuli <athavankanapuli@gmail.com>
2020-06-20 06:16:52 +0530

    Capture user input prompts

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,24 @@
 //
 
 use std::io;
-use std::io::Write;
+use std::io::{Read, Write};
 
 fn main() {
+    crossterm::terminal::enable_raw_mode().unwrap();
     let mut stdout = io::stdout();
-    stdout.write_all(b"hello world\n").unwrap();
+    //    stdout.write_all(b"hello world\n").unwrap();
+    let mut stdin = io::stdin().bytes();
+    loop {
+        write!(stdout, "Type something > ").unwrap();
+        stdout.flush().unwrap();
+        let c = char::from(stdin.next().unwrap().unwrap());
+        if c == 'q' {
+            write!(stdout, "You typed '{}' to quit\n\r", c).unwrap();
+            stdout.flush().unwrap();
+            break;
+        }
+        write!(stdout, "You typed '{}'\n\r", c).unwrap();
+        stdout.flush().unwrap();
+    }
+    crossterm::terminal::disable_raw_mode().unwrap();
 }
