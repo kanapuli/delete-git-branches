aab0cc7aba3d3f85c01ac5efc95fd89792998044
kanapuli <athavankanapuli@gmail.com>
2020-06-20 06:42:19 +0530

    Replace unwraps from main funtion to ? operator

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,27 @@
 use std::io;
 use std::io::{Read, Write};
 
-fn main() {
-    crossterm::terminal::enable_raw_mode().unwrap();
+fn main() -> Result<(), crossterm::ErrorKind> {
+    crossterm::terminal::enable_raw_mode()?;
     let mut stdout = io::stdout();
     //    stdout.write_all(b"hello world\n").unwrap();
     let mut stdin = io::stdin().bytes();
     loop {
-        write!(stdout, "Type something > ").unwrap();
-        stdout.flush().unwrap();
-        let c = char::from(stdin.next().unwrap().unwrap());
+        write!(stdout, "Type something > ")?;
+        stdout.flush()?;
+        let byte = match stdin.next() {
+            Some(byte) => byte?,
+            None => break,
+        };
+        let c = char::from(byte);
         if c == 'q' {
-            write!(stdout, "You typed '{}' to quit\n\r", c).unwrap();
-            stdout.flush().unwrap();
+            write!(stdout, "You typed '{}' to quit\n\r", c)?;
+            stdout.flush()?;
             break;
         }
-        write!(stdout, "You typed '{}'\n\r", c).unwrap();
-        stdout.flush().unwrap();
+        write!(stdout, "You typed '{}'\n\r", c)?;
+        stdout.flush()?;
     }
-    crossterm::terminal::disable_raw_mode().unwrap();
+    crossterm::terminal::disable_raw_mode()?;
+    Ok(())
 }
